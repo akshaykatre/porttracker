dae652f5f0767050c149b2eb4a73435bed020150
Akshay Katre <akshaykatre@gmail.com>
2021-07-12 23:29:44 +0200

    Stable data entry

--- createtable.py
+++ createtable.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("portfolio.db")
 conn.execute('DROP TABLE IF EXISTS PORTFOLIO')
 conn.execute('''CREATE TABLE PORTFOLIO(
-    ID INT PRIMARY KEY NOT NULL,
+    ID INT NOT NULL,
     SYMBOL TEXT NOT NULL, 
     STOCKNAME TEXT NOT NULL,
     UNITS INT NOT NULL,
--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,10 @@ from dash.dependencies import Input, Output, State
 from datetime import date
 import sqlite3
 import random 
+import dash_table
+import pandas 
+import dash_bootstrap_components as dbc 
+import time
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -22,47 +26,100 @@ fields = {
     "Units": "number"
 }
 
-app.layout = html.Div(style={'width':'10%', 'height':'100%','float':'left'},
-    children =
-    # [
-    #     dcc.Input(id="input_{}".format(k), type=v, placeholder="input {}".format(k))
-    #     for k,v in zip(fields.keys(), fields.values())
-    # ]
-    [ dcc.Input(id='input_Symbol', type='text', placeholder='input Symbol'),
-      dcc.Input(id='input_Stockname', type='text', placeholder='input Stockname'),
-      dcc.Input(id='input_Price', type='number', placeholder='input Price') ,
-      dcc.Input(id='input_Units', type='number', placeholder='input Units') ,
-    ]
-    + [dcc.DatePickerSingle(
+app.layout = dbc.Container([
+
+    dbc.Row(
+        dbc.Col(
+            [dcc.RadioItems(id='checklist', 
+                    options=[{"label": "Buy", "value": 'buy'}, {'label': 'Sell', 'value': 'sell'}], 
+                    labelStyle={"display": "inline-block"}
+                    )
+    ], width=4 )
+    ),
+    dbc.Row( 
+        dbc.Col(dcc.Input(id='input_Symbol', type='text', placeholder='input Symbol'), width=2)
+    ),
+    dbc.Row(
+        dbc.Col(dcc.Input(id='input_Stockname', type='text', placeholder='input Stockname'), width=2)
+    ),
+    dbc.Row(
+        dbc.Col(dcc.Input(id='input_Price', type='number', placeholder='input Price') , width=2)
+    ),
+    dbc.Row(
+        dbc.Col(dcc.Input(id='input_Units', type='number', placeholder='input Units'), width=2)
+    ),
+    dbc.Row(
+        dbc.Col(
+            [dcc.DatePickerSingle(
         id='my-date-picker-single',
         min_date_allowed=date(1995, 8, 5),
         max_date_allowed=date(2075, 12, 31),
         initial_visible_month=date(2021, 1, 1)
     )]
-    + [html.Button(id='submit-button',n_clicks=0, children='Submit')]
-    + [html.Div(id="out-all-types")]
-)
+    , width=2   )
+    ),
+    dbc.Row(
+        dbc.Col(html.Button(id='submit-button',n_clicks=0, children='Submit'),width=2)
+    ),
+    dbc.Row(
+        dbc.Col(html.Div(id="out-all-types"), width=2)
+    ),
+    dbc.Row(
+        dbc.Col(html.Div(id='tableloc'))
+    )
+
+
+])
 
 
 @app.callback(
     Output("out-all-types", "children"),
     Input('submit-button', 'n_clicks'),
+    State('checklist', 'value'), 
     [State("input_{}".format(k), "value") for k in fields.keys()],
     State('my-date-picker-single', 'date')
 )
-def cb_render(n_clicks, input_Symbol, input_Stockname, input_Price, input_Units, date):
-    try:
-        conn = sqlite3.connect("portfolio.db")
-        conn.execute('''INSERT INTO PORTFOLIO(ID, SYMBOL, STOCKNAME, UNITS, PRICE, DATE)
-                    VALUES({id}, '{symbol}', '{stockname}', {units}, {price}, '{dates}')'''.format(id=random.randint(0, 1000000) , 
-                            symbol=input_Symbol, stockname=input_Stockname, price=input_Price, units=input_Units, dates=date))
-        conn.commit()
-        conn.close()
-        return "Value inserted"
-    except Exception as e: 
-        pass
+def enterdata(n_clicks, buy_sell, input_Symbol, input_Stockname, input_Price, input_Units, date):
+    '''
+    Enter or remove data in the database based on the inputs
+    '''
+    
+    print(buy_sell, input_Price, input_Stockname, input_Symbol)
+    if buy_sell == 'buy': 
+        try:
+            print("Are we here?")
+            connection = sqlite3.connect("portfolio.db")
+            print("Connectionname: ", connection)
+            with connection as conn: 
+                print( "Its in connection")
+                conn.execute('''INSERT INTO PORTFOLIO(ID, SYMBOL, STOCKNAME, UNITS, PRICE, DATE)
+                            VALUES({id}, '{symbol}', '{stockname}', {units}, {price}, '{dates}')'''.format(id=hash(input_Symbol+input_Stockname), 
+                                    symbol=input_Symbol, stockname=input_Stockname, price=input_Price, units=input_Units, dates=date))
+                conn.commit()
+                print(conn)
+                conn.close()
+            return "Value inserted"
+        except Exception as e: 
+            print( "does it make exception? ", e )
+            pass
+    if buy_sell =='sell': 
+        return 'We are still working on this'
 
+@app.callback(
+    Output('tableloc', 'children'),
+    [Input('submit-button', 'n_clicks')]
+)
+def data_table(n_clicks):
+    time.sleep(2)
+    print("printed these")
 
+    connection = sqlite3.connect("portfolio.db")
+    with connection as conn2: 
+        df = pandas.read_sql_query("SELECT * from PORTFOLIO", conn2)
+        columns = [{'name': col, 'id': col} for col in df.columns]
+        data = df.to_dict('records')
+        #conn.close()
+    return dash_table.DataTable(id='tables', columns=columns, data=data)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
